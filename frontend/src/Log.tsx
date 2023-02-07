1401474a242553aaf8d663d5516a7807843452eb
kijube <kijubeofficial@gmail.com>
2023-02-07 09:30:49 +0100

    x

--- frontend/src/Log.tsx
+++ frontend/src/Log.tsx
@@ -10,30 +10,43 @@ export default function Log({
   const { buffer } = useLogStore()
   const { isExecuting, currentCommand } = useCurrentCommandStore()
 
+  function estimateSize(index: number) {
+    return buffer[index].type === "input" ? 30 : 20
+  }
+
   const virtualizer = useVirtualizer({
     count: buffer.length,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 20,
+    estimateSize,
   })
 
   return (
     <>
-      {virtualizer.getVirtualItems().map((virtualItem) => {
-        return (
-          <div
-            key={virtualItem.index}
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              transform: `translateY(${virtualItem.start}px)`,
-            }}
-          >
-            <LogLineItem line={buffer[virtualItem.index]} />
-          </div>
-        )
-      })}
+      <div
+        style={{
+          height: `${virtualizer.getTotalSize()}px`,
+          width: "100%",
+          position: "relative",
+        }}
+      >
+        {virtualizer.getVirtualItems().map((virtualItem) => {
+          return (
+            <div
+              key={virtualItem.index}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              }}
+            >
+              <LogLineItem line={buffer[virtualItem.index]} />
+            </div>
+          )
+        })}
+      </div>
       {isExecuting && (
         <p>
           <span className="text-neutral-500">Executing</span> {currentCommand}
@@ -45,7 +58,7 @@ export default function Log({
 
 function LogLineItem({ line }: { line: LogLine }) {
   if (line.type === "input") {
-    return <p className="text-neutral-500">{line.command}</p>
+    return <p className="text-neutral-500 pt-2">{line.command}</p>
   } else if (line.type === "output") {
     return <p>{line.output}</p>
   } else if (line.type === "error") {
